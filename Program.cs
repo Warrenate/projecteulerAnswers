4c54a2854c7fbdc6f3b50fca461cb3453bdc9b08
Warrenate <jtlxg@sina.com>
2016-05-18 09:04:30 +0800

    1. Find the sum of all the multiples of 3 or 5 below 1000.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace projecteulerAnswers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(UtilityAnswers.GetMultiplesOf3Or5());
+        }
+    }
+}
---
+++ UtilityAnswers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace projecteulerAnswers
+{
+    class UtilityAnswers
+    {
+        //1. Find the sum of all the multiples of 3 or 5 below 1000.
+        public static int GetMultiplesOf3Or5()
+        {
+            int result = 0;
+            for (int i = 3; i < 1000; i += 3)
+            {
+                result += i;
+            }
+            for (int i = 5; i < 1000; i += 5)
+            {
+                result += i;
+            }
+            for (int i = 15; i < 1000; i += 15)
+            {
+                result -= i;
+            }
+            return result;
+        }
+    }
+}
