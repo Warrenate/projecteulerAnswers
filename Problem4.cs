a8001accd156a98f8cb5f5fe49dcc640fb50691c
jtlxg <jtlxg@sina.com>
2016-05-23 20:49:25 +0800

    add Problem4.

---
+++ Problem4.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace projecteulerAnswers
+{
+    /// <summary>
+    /// 4. Largest palindrome product https://projecteuler.net/problem=4
+    /// </summary>
+    class Problem4
+    {
+        /// <summary>
+        /// get the largest palindrome product of two same digit nums(inefficient).
+        /// </summary>
+        /// <param name="digits">digit count</param>
+        /// <param name="factor1">factor 1</param>
+        /// <param name="factor2">factor 2</param>
+        /// <returns></returns>
+        public static ulong GetLargestPalindromeProductOfTwoNumInDigitsOf(byte digits, out ulong factor1, out ulong factor2)
+        {
+            factor1 = factor2 = 0;
+            if (digits == 1)
+            {
+                return 0;
+            }
+            ulong minNum = (ulong)Math.Pow(10, digits - 1);
+            ulong maxNum = minNum * 10;
+            ulong result = 0;
+            ulong tempResult;
+            for (ulong i = maxNum - 1; i > minNum; i--)
+                for (ulong j = maxNum - 1; j > minNum; j--)
+                {
+                    tempResult = i * j;
+                    if (IsPalindromeNumber(tempResult))
+                    {
+                        if (result < tempResult)
+                        {
+                            result = tempResult;
+                            factor1 = j;
+                            factor2 = i;
+                        }
+                    }
+                }
+            return result;
+        }
+
+        public static ulong GetLargestPalindromeProductOfTwoNumInDigitsOf(byte digits)
+        {
+            ulong factor1;
+            ulong factor2;
+            return GetLargestPalindromeProductOfTwoNumInDigitsOf(digits, out factor1, out factor2);
+        }
+
+        public static bool IsPalindromeNumber(ulong num)
+        {
+            string numToStr = num + string.Empty;
+            char[] tempArr = numToStr.ToCharArray();
+            Array.Reverse(tempArr);
+            string newStr = new string(tempArr);
+            return newStr == numToStr;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -15,7 +15,12 @@ namespace projecteulerAnswers
             //Console.WriteLine(Problem2.GetFabValueOf(50)); //too slow
             //Console.WriteLine(Problem2.GetEvenTermSumOfFibonacciIn(4000000));
 
-            Console.WriteLine(Problem3.GetLargestPrimeFactorOf(600851475143));
+            //Console.WriteLine(Problem3.GetLargestPrimeFactorOf(600851475143));
+
+            ulong factor1;
+            ulong factor2;
+            Console.WriteLine("{0} = {1} * {2}", Problem4.GetLargestPalindromeProductOfTwoNumInDigitsOf(3, out factor1, out factor2), factor1, factor2);
+            Console.WriteLine(Problem4.GetLargestPalindromeProductOfTwoNumInDigitsOf(3));
         }
     }
 }
